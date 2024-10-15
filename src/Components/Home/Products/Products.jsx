bef77cac434dbc6a1e60803fe253032ad1503e4b
TalhaJubaerPrantor <talhajubaer3121@gmail.com>
2024-10-15 12:36:11 +0600

    adding notes

--- src/Components/Home/Products/Products.jsx
+++ src/Components/Home/Products/Products.jsx
@@ -18,7 +18,16 @@ const Mid = () => {
                     <div className='prod'>
                         <img style={{width:"400px"}} src={product.img} st alt="" />
                         <h1>{product.name}</h1>
-                        <h3>BDT:{product.price}</h3>
+                        <div style={{display:"grid",gridTemplateColumns:"50% 50%"}}>
+                            <span><h3>BDT : {product.price}</h3></span>
+                            <span>
+                            <h4></h4>
+                            Top Notes:{product.top} <br />
+                            Heart Notes:{product.heart} <br />
+                            Base Notes:{product.base}
+                            </span>
+                        </div>
+                        
                         
                     </div>
                   ))  
